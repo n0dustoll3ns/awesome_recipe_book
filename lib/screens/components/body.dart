2906a7bd64301dc66ee839175fdd98ba2b12c05b
n0dustoll3ns <fominykh.dg@gmail.com>
2022-03-23 15:24:51 +0700

    separete categories to another file and class

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -3,6 +3,8 @@ import 'package:awesome_recipe_book/size_config.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
+import 'categories.dart';
+
 class Body extends StatelessWidget {
   const Body({Key key}) : super(key: key);
 
@@ -16,64 +18,3 @@ class Body extends StatelessWidget {
   }
 }
 
-class Categories extends StatefulWidget {
-  const Categories({Key key}) : super(key: key);
-
-  @override
-  State<Categories> createState() => _CategoriesState();
-}
-
-class _CategoriesState extends State<Categories> {
-  List<String> categories = [
-    "All",
-    "Indian",
-    "Mexican",
-    "Chinese",
-    "Uzbek",
-    "Vegan"
-  ];
-  int selectedIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: SizeConfig.defaultSize * 3.5,
-      child: ListView.builder(
-        itemCount: categories.length,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) => buildCategoryItem(index),
-      ),
-    );
-  }
-
-  Widget buildCategoryItem(int index) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          selectedIndex = index;
-        });
-      },
-      child: Container(
-        alignment: Alignment.center,
-        margin: EdgeInsets.only(left: SizeConfig.defaultSize * 2),
-        padding: EdgeInsets.symmetric(
-            horizontal: SizeConfig.defaultSize * 2,
-            vertical: SizeConfig.defaultSize / 2),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(
-            SizeConfig.defaultSize * 1.6,
-          ),
-          color: selectedIndex == index
-              ? Color.fromARGB(255, 235, 241, 234)
-              : Colors.transparent,
-        ),
-        child: Text(
-          categories[index],
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: selectedIndex == index ? kPrimaryColor : Color(0xFFC2C2B5),
-          ),
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/screens/components/categories.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+ import '../../constants.dart';
+import '../../size_config.dart';
+
+class Categories extends StatefulWidget {
+  const Categories({Key key}) : super(key: key);
+
+  @override
+  State<Categories> createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<String> categories = [
+    "All",
+    "Indian",
+    "Mexican",
+    "Chinese",
+    "Uzbek",
+    "Vegan"
+  ];
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: SizeConfig.defaultSize * 3.5,
+      child: ListView.builder(
+        itemCount: categories.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) => buildCategoryItem(index),
+      ),
+    );
+  }
+
+  Widget buildCategoryItem(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      child: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.only(left: SizeConfig.defaultSize * 2),
+        padding: EdgeInsets.symmetric(
+            horizontal: SizeConfig.defaultSize * 2,
+            vertical: SizeConfig.defaultSize / 2),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(
+            SizeConfig.defaultSize * 1.6,
+          ),
+          color: selectedIndex == index
+              ? Color.fromARGB(255, 235, 241, 234)
+              : Colors.transparent,
+        ),
+        child: Text(
+          categories[index],
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: selectedIndex == index ? kPrimaryColor : const Color(0xFFC2C2B5),
+          ),
+        ),
+      ),
+    );
+  }
+}
