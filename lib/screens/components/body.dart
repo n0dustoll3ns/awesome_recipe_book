0fd542dd8a03167dfcdf517dde741ddcefbea3d5
n0dustoll3ns <fominykh.dg@gmail.com>
2022-03-23 20:30:56 +0700

    .

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,7 +1,6 @@
 import 'package:awesome_recipe_book/constants.dart';
 import 'package:awesome_recipe_book/models/RecipeBundel.dart';
 import 'package:awesome_recipe_book/size_config.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
